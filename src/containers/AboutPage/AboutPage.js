e4b5c0090631d46729850dab8083f07959938cb8
Jari Whaith Nybäck <jari.nyback@gmail.com>
2019-11-14 23:13:58 +0200

    refactor: 💡 add about page content

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -35,48 +35,50 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <h1 className={css.pageTitle}>Experience unique homes and meet amazing locals</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+              <p>Placeholder</p>
             </div>
 
             <div className={css.contentMain}>
               <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
+                What is the main point of travelling? We at bunkbunk believe it is to immerse the
+                essence of the local cultures. And is there a better way for it than staying at real
+                local homes?
               </h2>
+              <h3 className={css.subtitle}>Become a bunkbunk host</h3>
 
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                Bunkbunk hosts are able to earn money by using any square meter of their actual home
+                to host travellers. In bunkbunk you can only host in your real home where you
+                actually live in. Bunkbunk reimagines travelling back to its roots: enabling
+                connections between locals and travellers.
               </p>
 
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
+              <h3 className={css.subtitle}>We make travelling easier and cheaper</h3>
 
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                Travellers are allowed to visit overnight for a relatively low price so it's perfect
+                for budget travellers and to anyone who wants to save money. Most of the bunkbunk
+                hosts are highly committed to provide high-class local tips for free and many of
+                them are happy to show around and cook local food with you.
               </p>
 
               <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
+                Bunkbunk makes Daytime Stays easier than ever
               </h3>
               <p>
-                Saunatime is brought to you by the good folks at{' '}
+                {/* Saunatime is brought to you by the good folks at{' '}
                 <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
                 to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
                 app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                get in touch! */}
+                Is there an easier way to earn some extra cash than letting travellers stay at your
+                home while you’re away or want to meet a new person during the day. Whether you're
+                at work, at school or on a day trip. Bunkbunk it literally prints money for you.
               </p>
               <p>
                 You can also checkout our{' '}
