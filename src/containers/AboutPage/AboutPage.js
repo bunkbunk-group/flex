a81c453c586f760ddcfd0d729a97f93360172b8d
Jari Whaith Nybäck <jari.nyback@gmail.com>
2019-11-15 18:12:02 +0200

    refactor: 💡 change about text and section cta

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -40,16 +40,37 @@ const AboutPage = () => {
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-            <p>Did you know that Finland has more than 7 million bunks - more than one bunk per person</p>
+              <p>
+                Did you know that Finland has more than 7 million bunks - more than one bunk per
+                person
+              </p>
             </div>
             <div className={css.contentMain}>
-            <p style={{fontWeight: 'bold'}}>What is the main point of travelling? We at bunkbunk believe it is to immerse the essence of the local cultures. And is there a better way for it than staying at real local homes?</p>
-            <h2>Become a bunkbunk host</h2>
-            <p>Bunkbunk hosts are able to earn money by using any square meter of their actual home to host travellers. In bunkbunk you can only host in your real home where you actually live in. Bunkbunk reimagines travelling back to its roots: enabling connections between locals and travellers.</p>
-            <h2>We make travelling easier and cheaper</h2>
-            <p>Travellers are allowed to visit overnight for a relatively low price so it's perfect for budget travellers and to anyone who wants to save money. Most of the bunkbunk hosts are highly committed to provide high-class local tips for free and many of them are happy to show around and cook local food with you.</p>
-            <h2>Bunkbunk makes daytime stays easier than ever</h2>
-            <p>Is there an easier way to earn some extra cash than letting travellers stay at your home while you’re away or want to meet a new person during the day. Whether you're at work, at school or on a day trip. Bunkbunk it literally prints money for you.</p>
+              <p style={{ fontWeight: 'bold' }}>
+                What is the main point of travelling? We at bunkbunk believe it is to immerse the
+                essence of the local cultures. And is there a better way for it than staying at real
+                local homes?
+              </p>
+              <h2>Go home. Host people. Earn money.</h2>
+              <p>
+                Bunkbunk hosts are able to earn money by using any square meter of their actual home
+                to host travellers. In bunkbunk you can only host in your real home where you
+                actually live in. Bunkbunk reimagines travelling back to its roots: enabling
+                connections between locals and travellers.
+              </p>
+              <h2>We make travelling easier and cheaper</h2>
+              <p>
+                Travellers are allowed to visit overnight for a relatively low price so it's perfect
+                for budget travellers and to anyone who wants to save money. Most of the bunkbunk
+                hosts are highly committed to provide high-class local tips for free and many of
+                them are happy to show around and cook local food with you.
+              </p>
+              <h2>Bunkbunk makes daytime stays easier than ever</h2>
+              <p>
+                Is there an easier way to earn some extra cash than letting travellers stay at your
+                home while you will be out of home anyway during the day. Whether you're at work, at
+                school or on a day trip. Bunkbunk literally prints money for you.
+              </p>
             </div>
           </div>
         </LayoutWrapperMain>
--- src/custom/organisms/SectionFullWidthCta.js
+++ src/custom/organisms/SectionFullWidthCta.js
@@ -1,48 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 import helsinkiImage from './images/huvilakatu.jpg';
 
 const Container = styled.a`
   position: relative;
-  max-width: 990px;
+  max-width: 1135px;
   margin: auto;
   display: grid;
   place-content: center;
-  background: 
-  linear-gradient(
-          rgba(0, 0, 0, 0.5), 
-          rgba(0, 0, 0, 0.5)
-        ),
-  url('${p => p.bgSrc}');
   background-position: center;
   background-size: cover;
-  height: 300px;
-  padding: '6rem 4rem';
   cursor: pointer;
 
   @media (min-width: 991px) {
     border-radius: 1rem;
   }
-  
 `;
 
-const InnerDiv = styled.div`
+const InnerDiv = styled.a`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
   font-weight: bolder;
   color: white;
-  padding: 2rem;
+  display: grid;
+  align-content: center;
+  padding-left: 6rem;
+`;
+
+const Image = styled.img`
+  filter: brightness(80%);
+  border-radius: 0.3rem;
+
+  &:hover {
+    filter: brightness(50%);
+  }
 `;
 
 const SectionFullWidthCta = () => {
+  const [hover, setHover] = useState(false);
   return (
     <Container
-      bgSrc={helsinkiImage}
       href="/s?address=Helsinki%2C%20Finland&bounds=60.3101563%2C25.01165441%2C60.11100041%2C24.77584986&mapSearch=true"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
-      <InnerDiv>
-        <p style={{ fontWeight: 'bold', fontSize: '30px', marginBottom: '20px' }}>browse</p>
-        <p style={{ fontSize: '60px', lineHeight: '60px' }}>bunks in Helsinki</p>
-      </InnerDiv>
+      <div style={{ padding: '0 2rem' }}>
+        <Image
+          src={helsinkiImage}
+          alt="Landing page CTA"
+          width="100%"
+          style={{
+            transitionDuration: '0.3s',
+            borderRadius: '0.3rem',
+            filter: hover ? 'brightness(50%)' : 'brightness(80%)',
+          }}
+        />
+        <InnerDiv>
+          <p style={{ fontWeight: 'bold', fontSize: '30px', marginBottom: '20px' }}>browse</p>
+          <p style={{ fontSize: '70px', fontWeight: 'bolder', lineHeight: '60px' }}>
+            bunks in Helsinki
+          </p>
+        </InnerDiv>
+      </div>
     </Container>
   );
 };
