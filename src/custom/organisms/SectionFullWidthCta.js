d7ea4aee6de68e186e96bfad816b52f8a2a2ba38
Jari Whaith Nybäck <jari.nyback@gmail.com>
2019-11-15 18:31:54 +0200

    refactor: 💡 removed emotion usage in section-cta

--- src/custom/organisms/SectionFullWidthCta.js
+++ src/custom/organisms/SectionFullWidthCta.js
@@ -1,55 +1,27 @@
 import React, { useState } from 'react';
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
 
 import helsinkiImage from './images/huvilakatu.jpg';
-
-const Container = styled.a`
-  position: relative;
-  max-width: 1135px;
-  margin: auto;
-  display: grid;
-  place-content: center;
-  background-position: center;
-  background-size: cover;
-  cursor: pointer;
-
-  @media (min-width: 991px) {
-    border-radius: 1rem;
-  }
-`;
-
-const InnerDiv = styled.a`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  font-weight: bolder;
-  color: white;
-  display: grid;
-  align-content: center;
-  padding-left: 6rem;
-`;
-
-const Image = styled.img`
-  filter: brightness(80%);
-  border-radius: 0.3rem;
-
-  &:hover {
-    filter: brightness(50%);
-  }
-`;
 
 const SectionFullWidthCta = () => {
   const [hover, setHover] = useState(false);
   return (
-    <Container
+    <a
       href="/s?address=Helsinki%2C%20Finland&bounds=60.3101563%2C25.01165441%2C60.11100041%2C24.77584986&mapSearch=true"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
+      style={{
+        maxWidth: '1135px',
+        margin: 'auto',
+        display: 'grid',
+        placeContent: 'center',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        cursor: 'pointer',
+      }}
     >
-      <div style={{ padding: '0 2rem' }}>
-        <Image
+      <div style={{ padding: '0 2rem', position: 'relative' }}>
+        <img
           src={helsinkiImage}
           alt="Landing page CTA"
           width="100%"
@@ -59,14 +31,27 @@ const SectionFullWidthCta = () => {
             filter: hover ? 'brightness(50%)' : 'brightness(80%)',
           }}
         />
-        <InnerDiv>
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            fontWeight: 'bolder',
+            color: 'white',
+            display: 'grid',
+            alignContent: 'center',
+            paddingLeft: '6rem',
+          }}
+        >
           <p style={{ fontWeight: 'bold', fontSize: '30px', marginBottom: '20px' }}>browse</p>
           <p style={{ fontSize: '70px', fontWeight: 'bolder', lineHeight: '60px' }}>
             bunks in Helsinki
           </p>
-        </InnerDiv>
+        </div>
       </div>
-    </Container>
+    </a>
   );
 };
 
